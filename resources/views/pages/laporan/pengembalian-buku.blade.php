bcfac09c6b264e07f2677d3ba2c33dd881c80982
fazeelmuhammadzuhdi <fazeelmuhammadzuhdi@gmail.com>
2023-06-01 20:38:39 +0700

    Update

--- resources/views/pages/laporan/pengembalian-buku.blade.php
+++ resources/views/pages/laporan/pengembalian-buku.blade.php
@@ -11,10 +11,7 @@
 </head>
 
 <body>
-    <h2 class="text-center mt-2">Perpustakaan</h2>
-    <h5 class="text-center mt-2">{{ $title }}</h5>
-    <hr style="border:1px solid black;">
-    <br>
+    @include('pages.laporan.header_laporan')
     <div class="container">
         <table>
             <tr>
